9a1042202e24653bf2f888a5619d3d599038c5b6
yamatatsu <the.kinnikuman@gmail.com>
2024-03-11 00:30:40 +0900

    refactoring as embedded software

--- packages/provisioning-node/src/index.ts
+++ packages/provisioning-node/src/index.ts
@@ -1,11 +1,15 @@
 import { randomUUID } from "node:crypto";
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
+import { setTimeout } from "node:timers/promises";
 import { mqtt } from "aws-iot-device-sdk-v2";
 import createConnection from "./createConnection";
 
 const DEVICE_CERT_PATH = `${__dirname}/../cert/cert.pem`;
-
 const decoder = new TextDecoder("utf8");
+
+let certificatePem: string | null = null;
+let certificateOwnershipToken: string | null = null;
+let provisioned = false;
 
 main();
 
@@ -16,44 +20,28 @@ async function main() {
 
 	await connection.connect();
 
-	if (!existsSync(DEVICE_CERT_PATH)) {
-		const { resultPromise: resultPromiseOfCreateFromCsr } =
-			await subscribeOnce<{
-				certificateOwnershipToken: "string";
-				certificateId: "string";
-				certificatePem: "string";
-			}>(
-				connection,
-				"$aws/certificates/create-from-csr/json/accepted",
-				"$aws/certificates/create-from-csr/json/rejected",
-			);
+	if (existsSync(DEVICE_CERT_PATH)) {
+		console.info("Cert already exists");
+	} else {
+		// subscribe
 
-		console.info("[create-cert] Subscribing");
+		await startToSubscribe(connection);
+
+		// publish
 
 		await connection.publish(
 			"$aws/certificates/create-from-csr/json",
 			{ certificateSigningRequest: csr },
 			mqtt.QoS.AtLeastOnce,
 		);
-
 		console.info("[create-cert] Published");
 
-		const resultOfCreateFromCsr = await resultPromiseOfCreateFromCsr;
-		console.info("[create-cert] Success");
-
-		const { resultPromise } = await subscribeOnce<{ thingName: "string" }>(
-			connection,
-			"$aws/provisioning-templates/MyFleetProvisioningTemplate/provision/json/accepted",
-			"$aws/provisioning-templates/MyFleetProvisioningTemplate/provision/json/rejected",
-		);
-
-		console.info("[provisioning] Subscribing");
+		await waitFor(() => certificateOwnershipToken !== null);
 
 		await connection.publish(
 			"$aws/provisioning-templates/MyFleetProvisioningTemplate/provision/json",
 			{
-				certificateOwnershipToken:
-					resultOfCreateFromCsr.certificateOwnershipToken,
+				certificateOwnershipToken: certificateOwnershipToken,
 				parameters: {
 					// TODO: これを preProvisioningHook とかで使うのかな。
 					SerialNumber: randomUUID(),
@@ -61,57 +49,79 @@ async function main() {
 			},
 			mqtt.QoS.AtLeastOnce,
 		);
-
 		console.info("[provisioning] Published");
 
-		const result = await resultPromise;
-		console.info("[provisioning] Success", { result });
+		await waitFor(() => provisioned);
 
-		writeFileSync(DEVICE_CERT_PATH, resultOfCreateFromCsr.certificatePem);
-		console.info("[provisioning] Saved cert");
+		if (certificatePem) {
+			writeFileSync(DEVICE_CERT_PATH, certificatePem);
+			console.info("[provisioning] Saved cert");
+		} else {
+			console.warn("[provisioning] cert is not in result");
+		}
 	}
 
 	await connection.disconnect();
 }
 
-async function subscribeOnce<T>(
-	connection: mqtt.MqttClientConnection,
-	successTopic: string,
-	failureTopic: string,
-): Promise<{ resultPromise: Promise<T> }> {
-	const subscribePromises: Promise<unknown>[] = [];
+async function startToSubscribe(connection: mqtt.MqttClientConnection) {
+	await connection.subscribe(
+		"$aws/certificates/create-from-csr/json/accepted",
+		mqtt.QoS.AtLeastOnce,
+		(topic, payload, dup, qos, retain) => {
+			console.log(
+				`[create-cert] Success. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`,
+			);
+			const resultOfCreateCert = JSON.parse(decoder.decode(payload));
+			certificateOwnershipToken = resultOfCreateCert.certificateOwnershipToken;
+			certificatePem = resultOfCreateCert.certificatePem;
+		},
+	);
+	await connection.subscribe(
+		"$aws/certificates/create-from-csr/json/rejected",
+		mqtt.QoS.AtLeastOnce,
+		(topic, payload, dup, qos, retain) => {
+			console.log(
+				`[create-cert] Failure. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`,
+			);
+			const message = decoder.decode(payload);
+			console.error(message);
+		},
+	);
+	console.info("[create-cert] Subscribing");
+	await connection.subscribe(
+		"$aws/provisioning-templates/MyFleetProvisioningTemplate/provision/json/accepted",
+		mqtt.QoS.AtLeastOnce,
+		(topic, payload, dup, qos, retain) => {
+			console.log(
+				`[provisioning] Success. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`,
+			);
+			const resultOfProvisioning = JSON.parse(decoder.decode(payload));
+			console.info("[provisioning] payload", { resultOfProvisioning });
+			provisioned = true;
+		},
+	);
+	await connection.subscribe(
+		"$aws/provisioning-templates/MyFleetProvisioningTemplate/provision/json/rejected",
+		mqtt.QoS.AtLeastOnce,
+		(topic, payload, dup, qos, retain) => {
+			console.log(
+				`[provisioning] Failure. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`,
+			);
+			const message = decoder.decode(payload);
+			console.error(message);
+		},
+	);
+	console.info("[provisioning] Subscribing");
+}
 
-	const resultPromise = new Promise<T>((resolve, reject) => {
-		subscribePromises.push(
-			connection.subscribe(
-				successTopic,
-				mqtt.QoS.AtLeastOnce,
-				(topic, payload, dup, qos, retain) => {
-					console.log(
-						`Success. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`,
-					);
-					resolve(JSON.parse(decoder.decode(payload)));
-				},
-			),
-		);
-
-		subscribePromises.push(
-			connection.subscribe(
-				failureTopic,
-				mqtt.QoS.AtLeastOnce,
-				(topic, payload, dup, qos, retain) => {
-					console.log(
-						`Failure. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`,
-					);
-					const message = decoder.decode(payload);
-					console.error(message);
-					reject(message);
-				},
-			),
-		);
-	});
-
-	await Promise.all(subscribePromises);
-
-	return { resultPromise };
+async function waitFor(fn: () => boolean, retryTimeout = 5, interval = 1000) {
+	for (const attemptCount of Array(retryTimeout).keys()) {
+		if (attemptCount > 0) {
+			await setTimeout(interval);
+		}
+		if (fn()) {
+			return;
+		}
+	}
 }
